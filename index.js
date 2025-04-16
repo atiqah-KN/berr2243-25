d07405d00829052ab96880ecbae5e3a6fc1285c5
atiqah-KN <nkatiqah25@gmail.com>
2025-04-16 19:38:17 +0800

    Initial commit: Setup documentation

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-const { MongoClient} = require('mongodb');
+const mongoose = require("mongoose");
 
-async function main() {
-    //Replace <connection-string> with your MongoDB URI
-    const uri = "mongodb://localhost:27017"
-    const client = new MongoClient(uri);
+const startTime = Date.now(); //Start time of the script
 
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB!");
-
-        const db = client.db("testdb");
-        const collection = db.collection("users");
-
-        //Insert a document
-        await collection.insertOne({ name: "Alice", age: 25 });
-        console.log("Document inserted!");
-
-        //Query the document
-        const result = await collection.findOne({ name: "Alice" });
-        console.log("Query result:", result);
-    } catch (err){
-        console.error("Error:", err);
-    } finally {
-        await client.close();
-    }
-}
-
-main();
+mongoose.connect("mongodb://localhost:27017/test", {
+    useNewUrIParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    const endTime = Date.now(); //End time of the script
+    console.log("Connected to MongoDB successfully!");
+    console.log('Connection time: ${(endTime - startTime} ms');
+})
+.catch(err => console.error("Error:", err));
